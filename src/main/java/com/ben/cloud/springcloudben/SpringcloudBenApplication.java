0fc1a3991f25aaac3d2d4f1c6b5117e7f1414dcf
caiqibin <543036863@qq.com>
2019-08-25 08:29:13 +0800

    springcloud ben init

---
+++ src/main/java/com/ben/cloud/springcloudben/SpringcloudBenApplication.java
@@ -0,0 +1,13 @@
+package com.ben.cloud.springcloudben;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudBenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudBenApplication.class, args);
+    }
+
+}
